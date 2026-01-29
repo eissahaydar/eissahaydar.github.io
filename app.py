e4f6167ec0417109ae1e5fec1436ac5514222e57
Eissa Haydar <157940671+eissahaydar@users.noreply.github.com>
2026-01-29 00:54:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 import time
-
-await micropip.install("z3-solver", index_urls="https://gitlab.com/api/v4/projects/56254198/packages/pypi/simple")
-
 from z3 import Solver, Real, sat
 
 from helper import *
